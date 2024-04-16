aff62d0d02249ceab90ead68c019791445d93d94
codesofC <inelusjudelin01@gmail.com>
2024-04-15 23:53:08 -0300

    Add access

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -1,62 +1,57 @@
-'use client'
+"use client";
 
-import { useState, useEffect, ChangeEvent, SyntheticEvent } from 'react'
+import { useState, useEffect, ChangeEvent, SyntheticEvent } from "react";
 import Image from "next/image";
 import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
-import { FormUserProps } from '@/types/index';
-import { useFirebase } from '@/lib/Firebase/useFirebase';
-import { useRouter } from 'next/navigation';
-import Loader from '@/components/Loader';
+import { FormUserProps } from "@/types/index";
+import { useFirebase } from "@/lib/Firebase/useFirebase";
+import { useRouter } from "next/navigation";
+import Loader from "@/components/Loader";
 
 const Login = () => {
-
   const [formUser, setFormUser] = useState<FormUserProps>({
     email: "",
-    password: ""
-  })
-  const [error, setError] = useState("")
+    password: "",
+  });
+  const [error, setError] = useState("");
+  const [access, setAccess] = useState(false)
 
-  const {
-    setIsLoading, 
-    isLoading, 
-    signInUser,
-    setUserId
-  } = useFirebase()
+  const { setIsLoading, isLoading, signInUser, setUserId } = useFirebase();
 
-  const router = useRouter()
+  const router = useRouter();
 
   useEffect(() => {
-    setIsLoading(false)
-  }, [])
+    setIsLoading(false);
+  }, []);
 
   // Handle the value of inputs elements onChange with the id tag
   const handleForm = (event: ChangeEvent<HTMLInputElement>) => {
-    setFormUser(prevState => ({
+    setFormUser((prevState) => ({
       ...prevState,
-      [event.target.id]: event.target.value
-    }))
-  }
+      [event.target.id]: event.target.value,
+    }));
+  };
 
-  //Submit the datas to connect the user 
+  //Submit the datas to connect the user
   const handleSubmit = (e: SyntheticEvent) => {
+    e.preventDefault();
 
-    e.preventDefault()
-
-    setIsLoading(true)
+    setIsLoading(true);
     signInUser(formUser.email, formUser.password)
-    .then((userCredential) => {
-      //Success connection, get the userId and redirect to home page
-      setFormUser({email: "", password: ""})
-      setUserId(userCredential.user.uid);
-      router.push("/")
-      setIsLoading(false)
-    }).catch(err => {
-      setIsLoading(false)
-      setError('Email ou Senha incorreto')
-    })
-  }
+      .then((userCredential) => {
+        //Success connection, get the userId and redirect to home page
+        setFormUser({ email: "", password: "" });
+        setUserId(userCredential.user.uid);
+        router.push("/");
+        setIsLoading(false);
+      })
+      .catch((err) => {
+        setIsLoading(false);
+        setError("Email ou Senha incorreto");
+      });
+  };
 
   return (
     <div className="relative w-full h-screen lg:grid lg:grid-cols-5">
@@ -71,36 +66,45 @@ const Login = () => {
               className="w-full h-full object-contain"
             />
           </div>
-          <form 
-            className="flex flex-col w-full gap-4"
-            onSubmit={handleSubmit}
-          >
+          <div className="flex flex-col items-center gap-1">
+            <span 
+              className="text-sm bg-principal p-1 rounded-sm cursor-pointer text-white"
+              onClick={() => setAccess(!access)}
+            >Accesso</span>
+            <div className={`${access ? 'flex' : 'hidden'} flex-col font-extralight text-xs p-3 bg-gray-200 rounded-sm`}>
+              <span>Email: xyz@gmail.com ou inelusjudelin01@gmail.com</span>
+              <span>Senha: 123456</span>
+            </div>
+          </div>
+          <form className="flex flex-col w-full gap-4" onSubmit={handleSubmit}>
             <div className="grid gap-2">
               <Label htmlFor="email">Email</Label>
               <Input
                 id="email"
                 type="email"
                 value={formUser.email}
-                required 
-                onChange={e => handleForm(e)}
+                required
+                onChange={(e) => handleForm(e)}
               />
             </div>
             <div className="grid gap-2">
               <div className="flex items-center">
                 <Label htmlFor="password">Senha</Label>
               </div>
-              <Input 
-                id="password" 
-                type="password" 
+              <Input
+                id="password"
+                type="password"
                 value={formUser.password}
-                required 
-                onChange={e => handleForm(e)}
+                required
+                onChange={(e) => handleForm(e)}
               />
             </div>
-            {error ? (<div className='flex text-sm text-red-600'>
-              Email ou senha incorreto
-            </div>) : null}
-            <Button type="submit" className="w-full">
+            {error ? (
+              <div className="flex text-sm text-red-600">
+                Email ou senha incorreto
+              </div>
+            ) : null}
+            <Button type="submit" className="w-full bg-principal">
               Login
             </Button>
           </form>
@@ -114,12 +118,14 @@ const Login = () => {
           height="1080"
           className="h-full w-full object-cover"
         />
-        <div className='absolute top-0 left-0 w-full h-full z-10 bg-[rgba(0,0,0,.7)]' />
+        <div className="absolute top-0 left-0 w-full h-full z-10 bg-[rgba(0,0,0,.7)]" />
       </div>
 
-      {isLoading ? <div className='absolute top-0 left-0 w-full h-full flex items-center justify-center z-20'>
-        <Loader />
-      </div> : null}
+      {isLoading ? (
+        <div className="absolute top-0 left-0 w-full h-full flex items-center justify-center z-20">
+          <Loader />
+        </div>
+      ) : null}
     </div>
   );
 };
